787893b28d1b723dd2edcd64622625a6aa7dd93f
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-05-18 11:08:43 -0400

    Made 'Hello World!' a header

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('<h1>Hello World!</h1>');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'), "!");
 });
