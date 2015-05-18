f1da5c58bf95caeeeb9678b2dd04e446290be156
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-05-18 13:47:16 -0400

    Added a local database, testing on web.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -8,6 +9,18 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.writeHead("200",{"Context-Type": "text/html"});
   fs.createReadStream("./homepage/index.html").pipe(response);
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
