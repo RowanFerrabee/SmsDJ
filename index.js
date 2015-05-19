934e7c372ee3f63088b019321b76ead069ad91b8
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-05-18 21:25:24 -0400

    Adding names and numbers into database

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var pg = require('pg');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
   response.writeHead("200",{"Context-Type": "text/html"});
@@ -20,6 +23,23 @@ app.get('/db', function (request, response) {
       else
        { response.send(result.rows); }
     });
+  });
+});
+
+app.post('/addUser', function(request, response) {
+  var name = request.body.name;
+  var number = request.body.number;
+
+  console.log("Name: ",name,"  Number: ",number);
+  var query = 'insert into directory (name, number) values ('+name+','+number+')';
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	client.query(query, function(err, result) {
+  			done();
+  			if (err) {
+  				console.error(err);
+  			}
+  		});
   });
 });
 
