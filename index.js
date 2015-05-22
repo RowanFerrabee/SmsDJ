6eea8ff19fa5c66c40a2896c27cb8a3e7f9adff9
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-05-21 21:45:27 -0400

    Updated Twilio phone number

--- index.js
+++ index.js
@@ -12,31 +12,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
   response.writeHead("200",{"Context-Type": "text/html"});
+  fs.createReadStream("./homepage/homepage.html").pipe(response);
+});
+
+app.post('/text', function(request,response) {
   //Send a text message
   client.sendMessage({
-
       to: '+16137154438', // Any number Twilio can deliver to
-      from: '+14432334021', // A number you bought from Twilio and can use for outbound communication
-      body: 'ping.' // body of the SMS message
+      from: '+16479315875', // A number you bought from Twilio and can use for outbound communication
+      body: 'Word to your mother.' // body of the SMS message
 
-  }, function(err, responseData) { //this function is executed when a response is received from Twilio
+  }, function(err, responseData) { //executed when a response is received from Twilio
 
-      if (!err) { // "err" is an error received during the request, if any
-
-          // "responseData" is a JavaScript object containing data received from Twilio.
-          // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-          // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-         console.log(responseData.from); // outputs "+14506667788"
-          console.log(responseData.body); // outputs "word to your mother."
+      if (!err) {
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
 
       } else {
         console.log(err);
       }
 
   });
-  fs.createReadStream("./homepage/homepage.html").pipe(response);
 });
+
+//app.post('call', function(request,response) {
+//
+//});
 
 app.get('/db', function (request, response) {
   response.writeHead("200",{"Context-Type": "text/html"});
