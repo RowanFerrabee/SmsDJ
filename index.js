3cc122d2e603c3ecd65381c726ddd57388705b90
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-05-23 09:02:03 -0400

    Fixed error in PSQL syntax

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/text', function(request,response) {
     });
   });*/
   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-    client.query("UPDATE Parties SET usergrp = '"+from+","+"'+usergrp WHERE partyid = "+PartyID, function (err, result) {
+    client.query("UPDATE Parties SET usergrp = '"+from+","+"' || usergrp WHERE partyid = "+PartyID, function (err, result) {
       done();
       if (err) { 
         console.error(err); response.send("Error: " + err);
