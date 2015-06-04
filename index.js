487f8042baaebcfe1a6121e02a4f2bb5485c8dac
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-06-03 23:26:04 -0400

    Fixing Spotify Errors

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+  var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-modify';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
